a6355bd33fef5765273a5934c2619ebacb1232d8
David Quintanilla <david.quinta415@gmail.com>
2019-10-27 03:58:16 -0600

    Upload Trejo

--- script.js
+++ script.js
@@ -196,7 +196,7 @@ function getName(name) {
 }
 
 function loadLabeledImages() {
-  const labels = ["Dave", "Richi", "Cesar", "Matus", "Luis"];
+  const labels = ["Dave", "Trejo", "Cesar", "Matus", "Luis"];
   return Promise.all(
     labels.map(async label => {
       const descriptions = [];
